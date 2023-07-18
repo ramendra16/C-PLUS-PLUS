cc9c9f0f01ceeeb67fc886436738c25475c71db5
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-07-18 18:36:28 +0530

    Datatype in c++

---
+++ Datatypes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    int a=5;
+    float b= 6.5;
+    char c='C';
+    bool d= true;
+    cout <<"Value of int is " <<a <<"\n" "Value of float is " <<b <<"\n" "Value of char is " <<c <<"\n" "Value of bool is " <<d;
+    return 0;
+}
