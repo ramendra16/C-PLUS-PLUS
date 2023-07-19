ab0f920a4ee78fb0d15d8d871530e1e7da6fce62
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-07-19 23:15:31 +0530

    Input output taking

---
+++ input_output.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout<< "Enter the value for num1 \n" ;
+    cin>> num1;
+
+    cout<< "Enter the value for num2 \n" ;
+    cin>> num2;
+
+    cout<< "The sum of the given number is: " <<num1+num2;
+    return 0;
+
+
+}
