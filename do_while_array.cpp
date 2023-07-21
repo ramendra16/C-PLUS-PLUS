ccb4f63da7f70cba49d95a8bd68286f6a1391984
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-07-21 14:55:58 +0530

    DO WHILE LOOP ARRAY

---
+++ do_while_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=0;
+    int marks[]={1,2,3,4};
+    
+     do
+    {
+        cout<<i<<endl;
+        i++;
+    } while (i<4);
+
+
+
+return 0;
+}
