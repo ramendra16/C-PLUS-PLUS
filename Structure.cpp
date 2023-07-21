c52ad1331a58884e4e7bcd2bedc6b28df8753a66
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-07-21 21:54:07 +0530

    STRUCTURE

---
+++ Structure.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+struct kocbe
+{
+    int kstrength;
+    char firstword;
+    float fee;
+}st;
+int main()
+{
+    st.kstrength =70;
+    st.firstword='k';
+    st.fee=120000;
+
+    cout<<st.kstrength<<endl;
+    cout<<st.firstword<<endl;
+    cout<<st.fee<<endl;
+
+
+    
+
+
+
+return 0;
+}
