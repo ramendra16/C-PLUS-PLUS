4771aaaeb5645efbcc1c8acf522e351f4cf01e2b
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-07-24 23:45:44 +0530

    CONVERT CALCIUS TO KELVIN

---
+++ ques5.cpp
@@ -0,0 +1,18 @@
+// Write a program in C++ to convert temperature in Celsius to Kelvin
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    float kel, cel;
+		cout << "\n\n Convert temperature in Celsius to Kelvin  :\n";
+		cout << "---------------------------------------------------\n";	
+    cout << " Input the temperature in Celsius : ";
+    cin >> cel;
+    kel = cel + 273.15;
+    cout << " The temperature in Celsius : " << cel << endl;
+    cout << " The temperature in Kelvin : " << kel << endl;
+	cout << endl;
+    return 0;
+}
