dbf633db10b0027a3f5f59e9e20f8cbc01166022
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-08-08 21:29:44 +0530

    C++

---
+++ union2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+union A
+{
+    int a,b,c;
+}o1;
+
+int main()
+{
+
+    o1.a=10;
+    o1.b=20;
+    o1.c=o1.a;
+    o1.a=o1.b;
+    o1.b=o1.c;
+    cout<<"a: ="<<o1.a<<endl<<"b: ="<<o1.b;
+
+return 0;
+}
