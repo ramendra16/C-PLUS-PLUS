f62f4d456338444ba7616e525bb3f236c0469c90
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-08-21 21:19:38 +0530

    C++

---
+++ AREA_CIRCLE_WITHOUT_RETURNTYPE_WITHOUT_PARAMETER.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void area()
+{
+    int r;
+    cout<<"enter radius: ";
+    cin>>r;
+    cout<<3.14*r*r;
+}
+
+int main()
+{
+
+    area();
+
+return 0;
+}
