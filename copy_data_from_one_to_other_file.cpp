5f03c92b07d911e4db927f7425c28d864dba3d34
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-09-14 11:44:56 +0530

    C++

---
+++ copy_data_from_one_to_other_file.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+fstream obj1;
+obj1.open("Destination.txt",ios::out);
+fstream obj2;
+obj2.open("Message.txt",ios::in);
+char ch;
+if(!obj1||!obj2)
+{
+cout<<"\n File could not be opened";
+exit(1);
+}
+ch=obj2.get();
+while(obj2.eof()==0)
+{
+obj1.put(ch);
+ch=obj2.get();
+}
+cout<<"\n Data copied from one file into another";
+return 0;
+
+}
