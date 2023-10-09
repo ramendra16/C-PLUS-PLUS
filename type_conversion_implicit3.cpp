038de0f02ddef3cfe875869522facc554cfd462d
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-10-09 12:49:19 +0530

    C++

---
+++ type_conversion_implicit3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+int num; // declare int type variable   
+double num2 = 15.25; // declare and assign the double variable   
+  
+// use implicit type conversion to assign a double value to int variable  
+num = num2;  
+cout << " The value of the int variable is: " << num << endl;  
+cout << " The value of the double variable is: " << num2 << endl;  
+return 0;  
+}  
