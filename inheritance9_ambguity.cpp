cf18452a56f46acdc35cb8b454e10006a6e2573b
RAMENDRA SRIVASTAVA <114799768+ramendra16@users.noreply.github.com>
2023-10-19 23:57:03 +0530

    C++

--- inheritance9_ambguity.cpp
+++ inheritance9_ambguity.cpp
@@ -1,39 +1,46 @@
 #include<iostream>
 using namespace std;
-class A
-{
-    public:
-    int x;
-    void ip()
-    {
-        cout<<"Enter x: ";
-        cin>>x;
-    }
-};
 
 class B
 {
-    public:
-    int y;
-    void ip()
-    {
-        cout<<"Enter y: ";
-        cin>>y;
-    }
+	protected:
+		int x;
+		public:
+			void get_dataB()
+			{
+				cout<<"\n enter value of x: ";
+				cin>>x;
+			}
 };
-
-class C: public A, public B
+class DB1 : public virtual B
 {
-    int s;
-    void opC()
-    {
-        cout<<"Sum is: ";
-        s=x+y;
-    }
+	protected:
+		int y;
+		public:
+			void get_dataB1(){
+				cout<<"\n enter value of y: ";
+				cin>>y;
+			}
 };
-
-main()
+class DB2 : public virtual B
 {
-    C obj
-
+	protected:
+		int z;
+		public:
+			void get_dataB2()
+			{
+				cout<<"\n enter value of z: ";
+				cin>>z;
+			}
+};
+int main() {
+    DB1 objDB1;
+    objDB1.get_dataB();  
+    objDB1.get_dataB1();
+    
+    DB2 objDB2;
+    objDB2.get_dataB();  
+    objDB2.get_dataB2(); 
+    
+    return 0;
 }
